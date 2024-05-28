e68d4dddd7064b3e53f9d8fcca59c0a111357d98
MURAT OZTURK <102192639+MURATOZTURK1773@users.noreply.github.com>
2024-05-28 18:31:25 -0400

    refactored

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
 window.onload = function () {
-  var images = document
-    .getElementsByClassName("content")[0]
-    .getElementsByTagName("img");
-  var currentIndex = 0;
+  function initializeSlideshow() {
+    let images = document.querySelectorAll("img:not(.header-img)");
+    let currentIndex = 0;
 
-  function showNextImage() {
-    images[currentIndex].classList.remove("active");
+    function showNextImage() {
+      images[currentIndex].classList.remove("active", "rotate");
+      images[currentIndex].style.display = "none";
 
-    currentIndex = (currentIndex + 1) % images.length;
+      currentIndex = (currentIndex + 1) % images.length;
 
-    images[currentIndex].classList.add("active");
+      images[currentIndex].classList.add("active");
+      images[currentIndex].style.display = "block";
+    }
+
+    function rotateImages() {
+      let previousIndex = (currentIndex - 1 + images.length) % images.length;
+      images[previousIndex].classList.remove("rotate");
+
+      images[currentIndex].classList.add("rotate");
+    }
+
+    showNextImage();
+
+    setInterval(function () {
+      showNextImage();
+    }, 4000);
+
+    setInterval(function () {
+      rotateImages();
+    }, 20000);
   }
 
-  setInterval(showNextImage, 4000);
+  initializeSlideshow();
 };
+
+function attachDropdownListener() {
+  document.getElementById("iletisim").addEventListener("change", function () {
+    var option = this.options[this.selectedIndex];
+    var href = option.getAttribute("data-href");
+    if (href) {
+      window.location.href = href;
+    }
+  });
+}
+
+document.getElementById("iletisim").addEventListener("change", function () {
+  var option = this.options[this.selectedIndex];
+  var href = option.getAttribute("data-href");
+  if (href) {
+    window.location.href = href;
+  }
+  this.selectedIndex = 0;
+});
